035ea58c907397ab1323ed140c3c6923f115ff4b
takase <takase@jig.jp>
2023-02-06 13:52:17 +0900

    Lspとtreesitterを自動インストール

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -382,11 +382,11 @@ local config = {
       return config -- return final config table
     end,
     treesitter = { -- overrides `require("treesitter").setup(...)`
-      -- ensure_installed = { "lua" },
+      -- ensure_installed = { "all" },
     },
     -- use mason-lspconfig to configure LSP installations
     ["mason-lspconfig"] = { -- overrides `require("mason-lspconfig").setup(...)`
-      -- ensure_installed = { "sumneko_lua" },
+      ensure_installed = { "sumneko_lua", "tsserver", "angularls", "rust_analyzer", "astro", "vimls" },
     },
     -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
     ["mason-null-ls"] = { -- overrides `require("mason-null-ls").setup(...)`
